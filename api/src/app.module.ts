3acffff511af084ad50a19f4c3306bdacb0a5330
duongle26 <leduong26298@gmail.com>
2020-06-12 17:20:13 +0700

    feat(api): config, typeorm, morgan, validation pipe, swagger

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,8 +1,28 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
+import * as morgan from 'morgan';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: 5432,
+      username: process.env.DB_USER_NAME,
+      password: process.env.DB_USER_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [join(__dirname, '**/**.entity{.ts,.js}')],
+      synchronize: true
+    })
+  ],
   controllers: [],
   providers: []
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): void {
+    consumer.apply(morgan('dev')).forRoutes('*');
+  }
+}
--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.setGlobalPrefix('/v1');
+  app.useGlobalPipes(new ValidationPipe());
+
+  const options = new DocumentBuilder().setTitle('Olymbooks API v1').addBearerAuth().build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('/v1', app, document);
+
   await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
