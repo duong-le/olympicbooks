1aa8abefc02c0a1293c030cd380a7ef39a249664
duongle26 <leduong26298@gmail.com>
2020-07-12 00:55:03 +0700

    feat(store): home module connects api

--- store/src/app/pages/home/home.component.ts
+++ store/src/app/pages/home/home.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
-import { CategoriesService } from '../categories/categories.service';
+import { combineLatest } from 'rxjs';
 import { Category } from 'src/app/shared/Interfaces/category.interface';
+import { CategoriesService } from '../categories/categories.service';
+import { Product } from 'src/app/shared/Interfaces/product.interface';
+import { ProductsService } from '../products/products.service';
 
 @Component({
   selector: 'app-home',
@@ -10,66 +13,30 @@ import { Category } from 'src/app/shared/Interfaces/category.interface';
 })
 export class HomeComponent implements OnInit {
   categories: Category[];
-  hotDeals;
-  recommendations;
+  hotDeals: Product[];
+  recommendations: Product[];
 
-  bookData = [
-    { title: 'Chiến tranh và hòa bình', price: 1500000 },
-    { title: 'Thần Thoại Bắc Âu', price: 1500000, new: true },
-    { title: 'Không gia đình', price: 1500000 },
-    {
-      title:
-        'Lập trình hướng đối tượng dành cho người mới bắt đầu học lập trình',
-      salePrice: 1000000,
-      price: 1500000,
-      sale: true
-    },
-    {
-      title: 'Vi Điều Khiển Và Ứng Dụng - Arduino Dành Cho Người Tự Học',
-      price: 1500000
-    },
-    { title: 'Khi Hơi Thở Hóa Thinh Không ', price: 1500000, hot: true }
-  ];
-
+  limit = 6;
   hero = 'assets/images/hero.jpg';
-
   banner = {
-    left: {
-      src: 'assets/images/community.jpg',
-      caption: 'Cộng Đồng'
-    },
-    right: {
-      src: 'assets/images/new-arrivals.jpg',
-      caption: 'Sản Phẩm Mới'
-    }
+    left: { src: 'assets/images/community.jpg', caption: 'Cộng Đồng' },
+    right: { src: 'assets/images/new-arrivals.jpg', caption: 'Sản Phẩm Mới' }
   };
 
   constructor(
     private titleService: Title,
-    private categoriesService: CategoriesService
+    private categoriesService: CategoriesService,
+    private productsService: ProductsService
   ) {
     this.titleService.setTitle('Trang chủ | Olymbooks');
-
-    this.hotDeals = this.bookData.map((el, idx) => ({
-      ...el,
-      id: idx + 1,
-      src: `https://picsum.photos/seed/${Math.floor(Math.random() * 1000)}/460`,
-      salePrice: 1000000,
-      sale: true
-    }));
-
-    this.recommendations = this.bookData.map((el, idx) => ({
-      ...el,
-      id: idx + 1,
-      src: `https://picsum.photos/seed/${Math.floor(Math.random() * 1000)}/460`
-    }));
   }
 
   ngOnInit() {
-    this.categoriesService.getManyCategories().subscribe(
-      (response) => {
-        this.categories = response;
-      },
+    combineLatest(
+      this.categoriesService.categories$,
+      this.productsService.getManyProducts({ limit: this.limit })
+    ).subscribe(
+      (response) => ([this.categories, this.hotDeals] = response),
       (error) => {}
     );
   }
