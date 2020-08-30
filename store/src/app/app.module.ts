1aa9c0c925f77670c2c2044de223fc899f0bf043
duongle26 <leduong26298@gmail.com>
2020-08-30 18:34:21 +0700

    fix: use vi as default locale

--- store/src/app/app.module.ts
+++ store/src/app/app.module.ts
@@ -3,8 +3,7 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { registerLocaleData } from '@angular/common';
-import en from '@angular/common/locales/en';
-import localeVi from '@angular/common/locales/vi';
+import vi from '@angular/common/locales/vi';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 
@@ -12,14 +11,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { SharedModule } from './shared/shared.module';
-import { NZ_I18N, en_US } from 'ng-zorro-antd/i18n';
+import { NZ_I18N, vi_VN } from 'ng-zorro-antd/i18n';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzConfig, NZ_CONFIG } from 'ng-zorro-antd/core/config';
 import { JwtInterceptor } from './shared/Interceptors/jwt.interceptor';
 import { ErrorInterceptor } from './shared/Interceptors/error.interceptor';
 
-registerLocaleData(en);
-registerLocaleData(localeVi, 'vi-VN');
+registerLocaleData(vi);
 
 const ngZorroConfig: NzConfig = {
   message: { nzDuration: 1000, nzTop: 60 }
@@ -41,7 +39,7 @@ const ngZorroConfig: NzConfig = {
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
     { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
-    { provide: NZ_I18N, useValue: en_US },
+    { provide: NZ_I18N, useValue: vi_VN },
     { provide: NZ_CONFIG, useValue: ngZorroConfig }
   ],
   bootstrap: [AppComponent]
