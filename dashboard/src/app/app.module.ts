2ae3963f78641bc9c26f1ffedf3f38986ace928c
duongle26 <leduong26298@gmail.com>
2020-08-30 20:04:08 +0700

    feat(dashboard): sider menu component

--- dashboard/src/app/app.module.ts
+++ dashboard/src/app/app.module.ts
@@ -9,16 +9,26 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IconModule } from './shared/icon.module';
+import { MenuComponent } from './shared/Components/layout/menu.component';
 
+import { NZ_I18N, vi_VN } from 'ng-zorro-antd/i18n';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
-import { NZ_I18N } from 'ng-zorro-antd/i18n';
-import { vi_VN } from 'ng-zorro-antd/i18n';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
 
 registerLocaleData(vi);
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [FormsModule, HttpClientModule, BrowserModule, BrowserAnimationsModule, AppRoutingModule, IconModule, NzLayoutModule],
+  declarations: [AppComponent, MenuComponent],
+  imports: [
+    FormsModule,
+    HttpClientModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    IconModule,
+    NzLayoutModule,
+    NzMenuModule
+  ],
   providers: [{ provide: NZ_I18N, useValue: vi_VN }],
   bootstrap: [AppComponent]
 })
