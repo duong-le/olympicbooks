146d1883f3d36f3b24f716f4e1ba6e4d9c091e16
duongle26 <leduong26298@gmail.com>
2020-08-31 16:08:39 +0700

    refactor(api): http request logger

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,8 +1,12 @@
 import { APP_GUARD } from '@nestjs/core';
-import { Module, NestModule, MiddlewareConsumer, Logger } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { join } from 'path';
+import { SqlFormat } from './shared/Loggers/sql-format.logger';
+import { HttpRequestLogger } from './shared/Loggers/http-request.logger';
+
 import { AuthModule } from './modules/auth/auth.module';
 import { ProductsModule } from './modules/products/products.module';
 import { CategoriesModule } from './modules/categories/categories.module';
@@ -14,8 +18,7 @@ import { OrdersModule } from './modules/orders/orders.module';
 import { DiscountsModule } from './modules/discounts/discounts.module';
 import { TransactionsModule } from './modules/transactions/transactions.module';
 import { ShippingsModule } from './modules/shippings/shippings.module';
-import { SqlFormat } from './shared/Loggers/sql-format.logger';
-import morgan from 'morgan';
+
 import { RolesGuard } from 'src/shared/Guards/roles.guard';
 import { Exist } from './shared/Validators/exist/exist.service';
 import { ArrayExist } from './shared/Validators/array-exist/array-exist.service';
@@ -47,7 +50,6 @@ import { ArrayExist } from './shared/Validators/array-exist/array-exist.service'
     ShippingsModule
   ],
   providers: [
-    Logger,
     Exist,
     ArrayExist,
     {
@@ -57,13 +59,9 @@ import { ArrayExist } from './shared/Validators/array-exist/array-exist.service'
   ]
 })
 export class AppModule implements NestModule {
-  constructor(private logger: Logger) {}
-
   configure(consumer: MiddlewareConsumer): void {
     if (process.env.NODE_ENV !== 'PRODUCTION') {
-      consumer
-        .apply(morgan('dev', { stream: { write: (message) => this.logger.log(message.substring(0, message.lastIndexOf('\n')), 'HTTP Request') } }))
-        .forRoutes('*');
+      consumer.apply(HttpRequestLogger).forRoutes('*');
     }
   }
 }
