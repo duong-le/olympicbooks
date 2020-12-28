f9c1c65cd4ab0d1876c56d41f056e21ae41494d4
duong-le <leduong26298@gmail.com>
2020-12-28 23:18:02 +0700

    feat(store): Add facebook chat plugin

---
+++ store/src/app/shared/Providers/facebook.initializer.ts
@@ -0,0 +1,19 @@
+export function FacebookInitializer() {
+  return () =>
+    new Promise((resolve) => {
+      // Wait for facebook sdk to initialize before starting the angular app
+      window['fbAsyncInit'] = function () {
+        resolve(window['FB'].init({ xfbml: true, version: 'v9.0' }));
+      };
+
+      // Load facebook sdk script
+      (function (d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s);
+        js.id = id;
+        js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      })(document, 'script', 'facebook-jssdk');
+    });
+}
