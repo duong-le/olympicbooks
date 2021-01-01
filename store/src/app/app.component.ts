3f61b2a26cb7c253abb8c023ac40f96c11079444
duong-le <leduong26298@gmail.com>
2021-01-01 23:12:42 +0700

    feat(store): Add zalo chat widget

--- store/src/app/app.component.ts
+++ store/src/app/app.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) this.initZaloSDK();
+  }
+
+  initZaloSDK() {
+    let node = document.createElement('script');
+    node.src = 'https://sp.zalo.me/plugins/sdk.js';
+    node.type = 'text/javascript';
+    node.async = true;
+    document.getElementsByTagName('head')[0].appendChild(node);
+  }
+}
