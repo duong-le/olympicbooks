d28926713380265069e3055a95f18bc6e1201914
duong-le <leduong26298@gmail.com>
2021-03-22 22:36:08 +0700

    chore(store): Remove zalo chat widget

--- store/src/app/app.component.ts
+++ store/src/app/app.component.ts
@@ -1,28 +1,10 @@
-import { isPlatformBrowser } from '@angular/common';
-import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  zaloChatWidget: any = '<div class="zalo-chat-widget" data-oaid="3378651306062428036" data-style="2"></div>';
-
-  constructor(@Inject(PLATFORM_ID) private platformId: Object, private sanitized: DomSanitizer) {
-    this.zaloChatWidget = this.sanitized.bypassSecurityTrustHtml(this.zaloChatWidget);
-  }
-
-  ngOnInit() {
-    if (isPlatformBrowser(this.platformId)) this.initZaloSDK();
-  }
-
-  initZaloSDK() {
-    let node = document.createElement('script');
-    node.src = 'https://sp.zalo.me/plugins/sdk.js';
-    node.type = 'text/javascript';
-    node.async = true;
-    document.getElementsByTagName('head')[0].appendChild(node);
-  }
+export class AppComponent {
+  constructor() {}
 }
