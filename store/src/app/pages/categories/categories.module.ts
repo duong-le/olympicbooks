15a33933472bf354019a1378d0bc4f74e94781be
duong-le <leduong26298@gmail.com>
2021-05-25 03:12:41 +0700

    refactor(shop): Refactor category page using collection component

--- store/src/app/pages/categories/categories.module.ts
+++ store/src/app/pages/categories/categories.module.ts
@@ -1,14 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
 import { NzCardModule } from 'ng-zorro-antd/card';
-import { NzDividerModule } from 'ng-zorro-antd/divider';
 import { NzGridModule } from 'ng-zorro-antd/grid';
-import { NzListModule } from 'ng-zorro-antd/list';
-import { NzPaginationModule } from 'ng-zorro-antd/pagination';
-import { NzSelectModule } from 'ng-zorro-antd/select';
-import { NzSliderModule } from 'ng-zorro-antd/slider';
 
 import { IconModule } from '../../shared/Modules/icon.module';
 import { SharedModule } from '../../shared/Modules/shared.module';
@@ -22,15 +16,8 @@ import { CategoriesComponent } from './categories.component';
     SharedModule,
     IconModule,
     CategoriesRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,
     NzGridModule,
     NzCardModule,
-    NzDividerModule,
-    NzSelectModule,
-    NzSliderModule,
-    NzListModule,
-    NzPaginationModule,
     NzBreadCrumbModule
   ]
 })
