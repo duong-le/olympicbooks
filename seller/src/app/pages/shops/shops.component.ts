d06790444a08756db45e7897aa2dfef6b4edd45b
duong-le <leduong26298@gmail.com>
2021-05-28 00:03:17 +0700

    feat(seller): Display banner with alert message for inactive shop

--- seller/src/app/pages/shops/shops.component.ts
+++ seller/src/app/pages/shops/shops.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { ShopStatus } from '../../shared/Enums/shops.enum';
+import { Shop } from '../../shared/Interfaces/shop.interface';
+import { ShopsService } from './shops.service';
 
 @Component({
   selector: 'app-shops',
@@ -6,9 +11,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shops.component.scss']
 })
 export class ShopsComponent implements OnInit {
+  shop: Shop;
+
+  shopId: number;
   isCollapsed = false;
+  isBannerShowing = false;
+  bannerType: 'success' | 'info' | 'warning' | 'error';
+  bannerMessage: string;
+  bannerMap = {
+    [ShopStatus.UNLISTED]: {
+      bannerType: 'info',
+      bannerMessage: 'Cửa hàng đang ở chế độ tạm nghỉ. Click vào đây để thay đổi trạng thái của cửa hàng.'
+    },
+    [ShopStatus.UNAPPROVED]: {
+      bannerType: 'warning',
+      bannerMessage:
+        'Cửa hàng đang được xét duyệt. Vui lòng liên hệ bộ phận chăm sóc khách hàng để được hỗ trợ thêm.'
+    },
+    [ShopStatus.BANNED]: {
+      bannerType: 'error',
+      bannerMessage:
+        'Cửa hàng đã bị khoá do vi phạm nguyên tắc bán hàng. Vui lòng liên hệ bộ phận chăm sóc khách hàng để được hỗ trợ thêm.'
+    }
+  };
 
-  constructor() {}
+  constructor(private shopsService: ShopsService, private activatedRoute: ActivatedRoute) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe(({ shopId }) => {
+      this.shopId = shopId;
+      this.renderShopPage();
+    });
+  }
+
+  renderShopPage() {
+    this.shopsService.getOne(this.shopId).subscribe((response) => {
+      this.shop = response;
+
+      if (this.shop.status !== ShopStatus.ACTIVE) {
+        this.isBannerShowing = true;
+        this.bannerType = this.bannerMap[this.shop.status].bannerType as any;
+        this.bannerMessage = this.bannerMap[this.shop.status].bannerMessage;
+      }
+    });
+  }
 }
--- seller/src/app/pages/shops/shops.module.ts
+++ seller/src/app/pages/shops/shops.module.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { NzAlertModule } from 'ng-zorro-antd/alert';
+import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
 
@@ -9,6 +11,14 @@ import { ShopsComponent } from './shops.component';
 
 @NgModule({
   declarations: [ShopsComponent],
-  imports: [CommonModule, ShopsRoutingModule, IconsModule, NzLayoutModule, NzMenuModule]
+  imports: [
+    CommonModule,
+    ShopsRoutingModule,
+    IconsModule,
+    NzLayoutModule,
+    NzMenuModule,
+    NzAlertModule,
+    NzButtonModule
+  ]
 })
 export class ShopsModule {}
