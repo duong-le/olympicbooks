dd0c4a16a6fd9edea002179b0949af6e64bdf719
duong-le <leduong26298@gmail.com>
2021-06-10 16:44:26 +0700

    refactor: Rename portal dashboard to admin

--- admin/src/app/pages/layout/layout.component.ts
+++ admin/src/app/pages/layout/layout.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
-import { AuthenticationService } from '../../pages/authentication/authentication.service';
+import { AuthenticationService } from '../authentication/authentication.service';
 import { Authentication } from '../../shared/Interfaces/authentication.interface';
 import { ThemeService } from '../../shared/Providers/theme.service';
 
