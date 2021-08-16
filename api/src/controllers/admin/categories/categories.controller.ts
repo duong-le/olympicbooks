f1498ed44fc5509cb93e1bae0a86171e29937ca8
duong-le <leduong26298@gmail.com>
2021-08-16 13:37:32 +0700

    refactor(api): Move attribute related API to a separate controller

--- api/src/controllers/admin/categories/categories.controller.ts
+++ api/src/controllers/admin/categories/categories.controller.ts
@@ -1,9 +1,7 @@
 import {
-  BadRequestException,
   Body,
   Controller,
   Delete,
-  Get,
   NotFoundException,
   Param,
   ParseIntPipe,
@@ -26,14 +24,7 @@ import { CategoriesService } from '../../../services/categories.service';
 import { UploadOptions } from '../../../services/cloud-storage.service';
 import { File } from '../../../shared/Interfaces/file.interface';
 import { CategoriesController } from '../../store/categories/categories.controller';
-import {
-  CreateAttributeDto,
-  CreateAttributeValueDto,
-  CreateCategoryDto,
-  UpdateAttributeDto,
-  UpdateAttributeValueDto,
-  UpdateCategoryDto,
-} from './categories.dto';
+import { CreateCategoryDto, UpdateCategoryDto } from './categories.dto';
 
 @ApiTags('Admin Categories')
 @ApiBearerAuth()
@@ -105,123 +96,5 @@ export class AdminCategoriesController extends CategoriesController {
     if (!category) throw new NotFoundException(`Category ${id} not found`);
     await this.categoryRepository.softDelete(id);
     // if (category.imgUrl && category.imgName) await this.service.removeFile(category.imgName);
-  }
-
-  @ApiOperation({ summary: 'Retrieve many Attribute by category' })
-  @Get(':categoryId/attributes')
-  async getManyAttributes(@Param('categoryId', ParseIntPipe) categoryId: number): Promise<Attribute[]> {
-    return await this.attributeRepository.find({
-      where: { categoryId },
-      relations: ['attributeValues']
-    });
-  }
-
-  @ApiOperation({ summary: 'Retrieve one Attribute by category' })
-  @Get(':categoryId/attributes/:attributeId')
-  async getOneAttribute(
-    @Param('categoryId', ParseIntPipe) categoryId: number,
-    @Param('attributeId', ParseIntPipe) attributeId: number
-  ): Promise<Attribute> {
-    const attribute = await this.attributeRepository.findOne({
-      where: { categoryId, id: attributeId },
-      relations: ['attributeValues']
-    });
-    if (!attribute) throw new NotFoundException(`Attribute ${attributeId} not found`);
-
-    return attribute;
-  }
-
-  @ApiOperation({ summary: 'Create one Attribute by category' })
-  @Post(':categoryId/attributes')
-  async createOneAttribute(
-    @Param('categoryId', ParseIntPipe) categoryId: number,
-    @Body() dto: CreateAttributeDto
-  ): Promise<Attribute> {
-    try {
-      return await this.attributeRepository.save({ ...dto, categoryId });
-    } catch (error) {
-      throw new BadRequestException(error?.code === '23505' ? `Attribute ${dto.name} already exists` : '');
-    }
-  }
-
-  @ApiOperation({ summary: 'Update one Attribute by category' })
-  @Patch(':categoryId/attributes/:attributeId')
-  async updateOneAttribute(
-    @Param('categoryId', ParseIntPipe) categoryId: number,
-    @Param('attributeId', ParseIntPipe) attributeId: number,
-    @Body() dto: UpdateAttributeDto
-  ): Promise<Attribute> {
-    const attribute = await this.attributeRepository.findOne({ categoryId, id: attributeId });
-    if (!attribute) throw new NotFoundException(`Attribute ${attributeId} not found`);
-
-    return await this.attributeRepository.save({ ...attribute, ...dto });
-  }
-
-  @ApiOperation({ summary: 'Delete one Attribute by category' })
-  @Delete(':categoryId/attributes/:attributeId')
-  async deleteOneAttribute(
-    @Param('categoryId', ParseIntPipe) categoryId: number,
-    @Param('attributeId', ParseIntPipe) attributeId: number
-  ): Promise<void> {
-    const attribute = await this.attributeRepository.findOne({ categoryId, id: attributeId });
-    if (!attribute) throw new NotFoundException(`Attribute ${attributeId} not found`);
-
-    await this.attributeRepository.softDelete(attribute.id);
-  }
-
-  @ApiOperation({ summary: 'Create one Attribute Value by category' })
-  @Post(':categoryId/attributes/:attributeId/attribute-values')
-  async createOneAttributeValue(
-    @Param('categoryId', ParseIntPipe) categoryId: number,
-    @Param('attributeId', ParseIntPipe) attributeId: number,
-    @Body() dto: CreateAttributeValueDto
-  ): Promise<AttributeValue> {
-    const attribute = await this.attributeRepository.findOne({ categoryId, id: attributeId });
-    if (!attribute) throw new NotFoundException(`Attribute ${attributeId} not found`);
-
-    try {
-      return await this.attributeValueRepository.save({ ...dto, attributeId });
-    } catch (error) {
-      throw new BadRequestException(
-        error?.code === '23505' ? `Attribute value ${dto.value} already exists` : ''
-      );
-    }
-  }
-
-  @ApiOperation({ summary: 'Update one Attribute Value by category' })
-  @Patch(':categoryId/attributes/:attributeId/attribute-values/:attributeValueId')
-  async updateOneAttributeValue(
-    @Param('categoryId', ParseIntPipe) categoryId: number,
-    @Param('attributeId', ParseIntPipe) attributeId: number,
-    @Param('attributeValueId', ParseIntPipe) attributeValueId: number,
-    @Body() dto: UpdateAttributeValueDto
-  ): Promise<AttributeValue> {
-    const attributeValue = await this.service.getOneAttributeValueByCategory(
-      categoryId,
-      attributeId,
-      attributeValueId
-    );
-    if (!attributeValue) throw new NotFoundException(`Attribute Value ${attributeValueId} not found`);
-
-    if (dto?.value) attributeValue.value = dto.value;
-
-    return await this.attributeValueRepository.save(attributeValue);
-  }
-
-  @ApiOperation({ summary: 'Delete one Attribute Value by category' })
-  @Delete(':categoryId/attributes/:attributeId/attribute-values/:attributeValueId')
-  async deleteOneAttributeValue(
-    @Param('categoryId', ParseIntPipe) categoryId: number,
-    @Param('attributeId', ParseIntPipe) attributeId: number,
-    @Param('attributeValueId', ParseIntPipe) attributeValueId: number
-  ): Promise<void> {
-    const attributeValue = await this.service.getOneAttributeValueByCategory(
-      categoryId,
-      attributeId,
-      attributeValueId
-    );
-    if (!attributeValue) throw new NotFoundException(`Attribute Value ${attributeValueId} not found`);
-
-    await this.attributeValueRepository.softDelete(attributeValue.id);
   }
 }
--- api/src/controllers/admin/categories/categories.module.ts
+++ api/src/controllers/admin/categories/categories.module.ts
@@ -5,10 +5,11 @@ import { AttributeValue } from '../../../entities/attribute-value.entity';
 import { Attribute } from '../../../entities/attribute.entity';
 import { Category } from '../../../entities/categories.entity';
 import { CategoriesModule } from '../../store/categories/categories.module';
+import { AdminAttributesController } from './attributes.controller';
 import { AdminCategoriesController } from './categories.controller';
 
 @Module({
-  controllers: [AdminCategoriesController],
+  controllers: [AdminCategoriesController, AdminAttributesController],
   imports: [TypeOrmModule.forFeature([Category, Attribute, AttributeValue]), CategoriesModule]
 })
 export class AdminCategoriesModule {}
