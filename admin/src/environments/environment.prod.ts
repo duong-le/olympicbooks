d7114bc25a17bc4213548fc0c30fc0c607b9790b
duong-le <leduong26298@gmail.com>
2021-09-29 00:37:33 +0700

    chore: Correct environment URLs

--- admin/src/environments/environment.prod.ts
+++ admin/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api-3lg52fkejq-as.a.run.app/v1/admin',
-  storeUrl: 'https://store-3lg52fkejq-as.a.run.app'
+  apiUrl: 'https://api.olympicbooks.com/v1/admin',
+  storeUrl: 'https://olympicbooks.com'
 };
